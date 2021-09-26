accbb9e5546c99355dc6384aca1756a5a2398ede
Randall Hunt <randy@rhunt.com>
2021-09-25 22:12:06 -0500

    fix for clause handling

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,28 +2,29 @@ class ApplicationsController < ApplicationController
 
 def index
   clause = ""
-  if params[:search]
+  if params[:search].present?
     clause = "c.first_name LIKE '%#{params[:search]}%' OR c.last_name LIKE '%#{params[:search]}%' OR c.email LIKE '%#{params[:search]}%'"
   end
   render :json => {
-    total_applications: total_applications,
-    qualifying_applications: qualifying_applications
+    total_applications: total_applications(clause),
+    qualifying_applications: qualifying_applications(clause)
   }
 end
 
 def total_applications(clause)
-  sql = "SELECT COUNT(*) AS cnt FROM applications "
-  if clause
+  sql = "SELECT COUNT(*) AS cnt FROM applications a INNER JOIN clients c ON a.client_id=c.id"
+  if clause.present?
     sql = "#{sql} WHERE #{clause}"
   end
+  #sql
   records = ActiveRecord::Base.connection.execute(sql)
   records.first[0]
 end
 
 def qualifying_applications(clause)
-  sql = "SELECT * FROM credits INNER JOIN applications ON credits.client_id = applications.client_id WHERE credits.credit_score > 520 AND (applications.monthly_debt * 12) / applications.annual_income < .5"
-  if clause
-    sql = "#{sql} AND #{clause}"
+  sql = "SELECT * FROM credits cr INNER JOIN applications a ON cr.client_id = a.client_id INNER JOIN clients c ON cr.client_id=c.id WHERE cr.credit_score > 520 AND (a.monthly_debt * 12) / a.annual_income < .5"
+  if clause.present?
+    sql = "#{sql} AND (#{clause})"
   end
   records = ActiveRecord::Base.connection.execute(sql)
   records.to_a.size
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,21 +2,23 @@ class ClientsController < ApplicationController
 
 def index
   clause = ''
-  if params[:search]
+  if params[:search].present?
     clause = "c.first_name LIKE '%#{params[:search]}%' OR c.last_name LIKE '%#{params[:search]}%' OR c.email LIKE '%#{params[:search]}%'"
   end
 
+  #render :json => { clause: clause }
+
   render :json => {
     total_clients: total_clients(clause),
-    clients_with_application: clients_with_application(" WHERE #{clause}"),
-    clients_missing_data: clients_missing_data(" AND #{clause}"),
-    average_credit_score: average_credit_score(" WHERE #{clause}")
+    clients_with_application: clients_with_application(clause),
+    clients_missing_data: clients_missing_data(clause),
+    average_credit_score: average_credit_score(clause)
   }
 end
 
 def total_clients(clause)
   sql = "SELECT COUNT(*) AS cnt FROM clients c"
-  if clause
+  if clause.present?
     sql = "#{sql} WHERE #{clause}"
   end
   records = ActiveRecord::Base.connection.execute(sql)
@@ -25,7 +27,7 @@ end
 
 def clients_with_application(clause)
   sql = "SELECT DISTINCT(a.client_id) AS cnt FROM applications a INNER JOIN clients c ON a.client_id=c.id"
-  if clause
+  if clause.present?
     sql = "#{sql} WHERE #{clause}"
   end
   records = ActiveRecord::Base.connection.execute(sql)
@@ -34,8 +36,8 @@ end
 
 def clients_missing_data(clause)
   sql = "SELECT c.first_name,  c.last_name, c.email, r.credit_score FROM clients c LEFT JOIN credits r on c.id=r.client_id  WHERE r.client_id IS NULL"
-  if clause
-    sql = "#{sql} AND #{clause}"
+  if clause.present?
+    sql = "#{sql} AND (#{clause})"
   end
   records = ActiveRecord::Base.connection.execute(sql)
   records.to_a.size
@@ -43,8 +45,8 @@ end
 
 def average_credit_score(clause)
   sql = "SELECT AVG(r.credit_score) AS avg FROM credits r INNER JOIN clients c ON c.id=r.client_id"
-  if clause
-    sql = "#{sql} WHERE #{clause}"
+  if clause.present?
+    sql = "#{sql} WHERE (#{clause})"
   end
   records = ActiveRecord::Base.connection.execute(sql)
   records.first[0]
